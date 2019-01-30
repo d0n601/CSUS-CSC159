4571de9d29cef26b0bf699d15fa4fed32457a7c1
KatyaNiosi <plimmi.1@gmail.com>
2019-01-30 14:24:27 -0800

    Dots just going hella fast now.

--- phase0/prep4/main.c
+++ phase0/prep4/main.c
@@ -14,16 +14,12 @@
 
 #define VID_MASK 0x0f00     // foreground bright white, background black.
 #define TIMER_INTR 32       // TIMER_INTR constant identifier.
-
-
-
-/*Type: Ptr to function take no parameters, return nothing. An ISR */
-typedef void (*PFV)(void);
-
-clock_t tick_count = 0; /* Count timer ticks, whatever the rate. (from service.c) */ 
+#define PIC_MASK 0x21       // Programmable Interrupt Controller I/O.
+#define MASK ~0x01          // Mask for Programmagle Interrupt Controller.
+#define PIC_CONTROL 0x20    // Programmable Interrupt Controller I/O.
+#define TIMER_DONE 0x60     // Sent to PIC control when timer intr service done.
 
 __BEGIN_DECLS
-/* first-level handler entry (written in assembly) */
 extern void TimerEntry(void);
 void TimerCode(void);
 __END_DECLS
@@ -31,48 +27,43 @@ __END_DECLS
 
 
 void TimerCode() {
-  /* Output a character once every second. */  
-  if( 0 == (++tick_count % CLK_TCK) ) {
-    cons_putchar('.');  
-  }
-  /**
-   * Dismiss the timer interrupt. Send a "specific End-Of_interrupt
-   * for IRQ 0" command to the Master Interrupt Control Unit.
-   */ 
-  outportb(ICU0_IOBASE, SPECIFIC_EOI(IRQ_TIMER));
+  
+  static int count = 0;
+
+  count++;
+
+  cons_putchar('.');
+
+  outportb(PIC_CONTROL, TIMER_DONE);
+
 }
 
 
 
 int main() {
  
+  char ch;
   struct i386_gate *intr_table;
 
-  /* Find out where FLAMES placed the IDT array; */
   intr_table = get_idt_base();
 
-   // set_exception_handler(IRQ_VECTOR(IRQ_TIMER), TimerEntry);
-   fill_gate(&intr_table[TIMER_INTR], (int)TimerEntry, get_cs(), ACC_INTR_GATE, 0);
+  fill_gate(&intr_table[TIMER_INTR], (int)TimerEntry, get_cs(), ACC_INTR_GATE, 0);
 
+  outportb(PIC_MASK, MASK);
 
-  /**
-   * Unmast IRQ 0 (the timer interrupt) while keeping all other IRQs
-   * masked (ie, disabled) on the master i8259 PIC (interrupt control 
-   * unit zero). Note, a 0 bit = enable.
-   */
-  outportb(ICU0_IOBASE+1, ~ 0x01); /* ~0x10 = 1111110 (bitwise NOT)  */
+  asm("sti");
 
-  /* We're ready to enable interrupts and start handing the timer.
-   * Loop waiting for a keypress on the console, getting interrupted 
-   * occasionally.
-   */
-  EI(); // REMOVE FOR OS CODE!!!
-  while(0==cons_kbhit()) {
-    IO_DELAY();
+  while(1) {
+    if( cons_kbhit() ) {
+      ch = cons_getchar();
+      if(ch=='b') {
+        breakpoint();
+      }
+      else{
+        cons_printf("thanks for pressing a key\n");
+      }
+    }  
   }
 
-  (void)cons_getchar(); /* Eat the keypress.  */
-
-  /* When we exit, FLAMES will disable interrupts for us. */
   return 0;
 }
