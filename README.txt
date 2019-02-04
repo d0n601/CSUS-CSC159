076c66e10015a987286378a2bcb189872c758c4d
Ryan Kozak <ryan@orcuttbay.com>
2019-02-04 13:55:23 -0800

    Added README. Finished phase0.

---
+++ README.txt
@@ -0,0 +1,5 @@
+# California State University Sacramento #
+### CSC 159 ###
+
+These are the phases for CSC/CPE 159 Operating System Pragmatics.
+
--- phase0/prep4/main.c
+++ phase0/prep4/main.c
@@ -13,10 +13,11 @@
 #define VID_MASK 0x0f00     // foreground bright white, background black.
 #define TIMER_INTR 32       // TIMER_INTR constant identifier.
 #define PIC_MASK 0x21       // Programmable Interrupt Controller I/O.
-#define MASK ~0x01          // Mask for Programmagle Interrupt Controller.
+#define MASK ~0x01          // Mask for Programmable Interrupt Controller.
 #define PIC_CONTROL 0x20    // Programmable Interrupt Controller I/O.
 #define TIMER_DONE 0x60     // Sent to PIC control when timer intr service done.
 #define LOOP 1666666      // LOOP to time 1 second (LOOP x .6 microseconds)
+
 
 __BEGIN_DECLS
 extern void TimerEntry(void);
@@ -24,19 +25,17 @@ __END_DECLS
 
 
 void TimerCode() {
-  int i; 
+
   static int count = 0;
 
   count++;
-  
-  for(i = 0; i < LOOP; i++) asm("inb $0x80"); // each inb delays CPU .6 microsecond
 
-  cons_putchar('.');
+  if( count % 300 == 0 ) cons_printf("\nRyan Kozak (press 'b' to return to GDB)\n");
+  else if( count % 50 == 0 ) cons_putchar('.');
 
   outportb(PIC_CONTROL, TIMER_DONE);
 
 }
-
 
 
 int main() {
@@ -53,16 +52,17 @@ int main() {
   asm("sti");
 
   while(1) {
-    if( cons_kbhit() ) {
-      ch = cons_getchar();
-      if(ch=='b') {
-        breakpoint();
-      }
-      else{
-        cons_printf("thanks for pressing a key\n");
-      }
-    }  
-  }
 
+    if( cons_kbhit() ) {
+
+      ch = cons_getchar();
+
+      if(ch=='b')breakpoint();
+
+      else cons_printf("\n%c pressed\n");
+
+    }
+
+  }
   return 0;
 }
