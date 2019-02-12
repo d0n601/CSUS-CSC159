6c9fe1c9e13b19ffb442236a5d8083e6711a6c48
Ryan Kozak <ryan@orcuttbay.com>
2019-02-11 18:32:51 -0800

    I think I finished the queue data structures and the assembly file now...

--- phase1/k-lib.c
+++ phase1/k-lib.c
@@ -10,37 +10,43 @@ void Bzero(char *p, int bytes) {
 }
 
 int QisEmpty(q_t *p) { // return 1 if empty, else 0
-   return 1;
+   return p->size == 0;
 }
 
 int QisFull(q_t *p) { // return 1 if full, else 0
-   return 0;
+   return p->size == Q_SIZE;
 }
 
-// dequeue, 1st # in queue; if queue empty, return -1
-// move rest to front by a notch, set empty spaces -1
+// Dequeue, 1st # in queue; if queue empty, return -1
+// Move rest to front by a notch, set empty spaces -1
 int DeQ(q_t *p) { // return -1 if q[] is empty
-   int ..., ...
+
+   int i, x;
 
    if(QisEmpty(p)) {
-      ...
+      return -1;
    }
 
-   ...
-   ...
-   ...
-   ...
-   ...
+   x = p->q[0];
+
+   p->size--;
+
+   for(i = 0; i < p->size-1; i++){
+      p->q[i] = p->q[i+1];
+   }
+
+   return x;
+
 }
 
-// if not full, enqueue # to tail slot in queue
+// If not full, enqueue # to tail slot in queue.
 void EnQ(int to_add, q_t *p) {
    if(QisFull(p)) {
       cons_printf("Panic: queue is full, cannot EnQ!\n");
-      ...
    }
-
-   ...
-   ...
+   else {
+      p->q[p->size] = to_add;
+      p->size++;
+   }
 }
 
--- phase1/k-sr.c
+++ phase1/k-sr.c
@@ -15,12 +15,14 @@ void NewProcSR(func_p_t p) {  // arg: where process code starts
 
    int pid;
 
-   if( QisEmpty(pid_q) ) {     // May occur if too many been created.
+   if( QisEmpty(&pid_q) ) {     // May occur if too many been created.
       cons_printf("Panic: no more process!\n");
       breakpoint();  // cannot continue
    }
 
-   ...                                       // alloc PID (1st is 0)
+   pid = DeQ(&ready_q);                      // alloc PID (1st is 0)
+
+   ...
    ...                                       // clear PCB
    ...                                       // clear stack
    ...                                       // change process state
@@ -28,26 +30,26 @@ void NewProcSR(func_p_t p) {  // arg: where process code starts
    if(pid > 0) ...                           // queue to ready_q if > 0
 
    // point trapframe_p to stack & fill it out
-   pcb[run_pid].trapframe_p = ...                // point to stack top
-   pcb[run_pid].trapframe_p--;                   // lower by trapframe size
-   pcb[run_pid].trapframe_p->efl = EF_DEFAULT_VALUE|EF_INTR; // enables intr
-   pcb[run_pid].trapframe_p->cs = get_cs();                  // dupl from CPU
-   pcb[run_pid].trapframe_p->eip =                           // set to code
+   pcb[run_pid].trapframe_p = ...                             // point to stack top
+   pcb[run_pid].trapframe_p--;                                // lower by trapframe size
+   pcb[run_pid].trapframe_p->efl = EF_DEFAULT_VALUE|EF_INTR;  // enables intr
+   pcb[run_pid].trapframe_p->cs = get_cs() ;                  // dupl from CPU
+   pcb[run_pid].trapframe_p->eip = (int)p;                    // set to code
 }
 
 
 /* count run_count and switch if hitting time slice */
 void TimerSR(void) {
 
-   outportb(PIC_CONTROL, TIMER_DONE);        // notify PIC timer done
+   outportb(PIC_CONTROL, TIMER_DONE);    // Notify PIC timer done.
 
-   pcb[run_pid].run_count++;                // count up run_count
-   pcb[run_pid].total_count++;              // count up total_count
+   pcb[run_pid].run_count++;             // Count up run_count.
+   pcb[run_pid].total_count++;           // Count up total_count.
 
-   if(run_count % TIME_SLICE == 0) {       // if runs long enough
-      ...                                    // move it to ready_q
-      ...                                    // change its state
-      run_pid = NONE;                        // running proc = NONE
+   if(run_count % TIME_SLICE == 0) {     // If runs long enough.
+      EnQ(run_pid, &ready_q);            // Move it to ready_q.
+      pcb[run_pid].state = READY;       // Change its state.
+      run_pid = NONE;                   // Running proc = NONE.
    }
 }
 
